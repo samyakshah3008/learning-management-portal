bddb5307344741e3758dd5d3d92bfda22723d33c
Samyak Shah <samyakshah3008@gmail.com>
2024-09-02 22:03:50 +0530

    feat: user schema add

---
+++ server/models/user.model.ts
@@ -0,0 +1,77 @@
+import bcrypt from "bcryptjs";
+import { Document, Model, Schema, model } from "mongoose";
+import { emailRegex } from "../constants/regex";
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  avatar: {
+    public_id: string;
+    url: string;
+  };
+  role: string;
+  isVerified: boolean;
+  courses: Array<{ courseId: string }>;
+  comparePassword: (password: string) => Promise<boolean>;
+}
+
+const userSchema: Schema<IUser> = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name."],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your email."],
+      validate: {
+        validator: function (value: string) {
+          return emailRegex.test(value);
+        },
+        message: "Please enter a valid email. ",
+      },
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your password"],
+      minlength: [6, "Password must be at least 6 characters. "],
+      select: false,
+    },
+    avatar: {
+      public_id: String,
+      url: String,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    courses: [
+      {
+        courseId: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+userSchema.pre<IUser & Document>("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (
+  userInputPassword: string
+): Promise<boolean> {
+  return await bcrypt.compare(userInputPassword, this.password);
+};
+
+const User: Model<IUser> = model("User", userSchema);
+
+export { User };
