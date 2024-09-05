70235bf4c2f90fda461e63050bf784c4c328460f
Samyak Shah <samyakshah3008@gmail.com>
2024-09-06 00:06:19 +0530

    signin signout routes add

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import {
   registratingUser,
+  signInUser,
+  signOutUser,
   verifyAndCreateUser,
 } from "../controllers/user.controller";
+import { isAuthenticated } from "../middleware/auth";
 
 const router = Router();
 
 router.post("/signup", registratingUser);
 router.post("/signup/verify-code", verifyAndCreateUser);
+router.post("/signin", signInUser);
+router.get("/signout", isAuthenticated, signOutUser);
 
 export default router;
