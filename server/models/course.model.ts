7e20237643f77f7a059658e28d99ac06dec386d6
Samyak Shah <samyakshah3008@gmail.com>
2024-09-06 22:54:28 +0530

    course schema design

---
+++ server/models/course.model.ts
@@ -0,0 +1,94 @@
+import { model, Model, Schema } from "mongoose";
+import {
+  IComment,
+  ICourse,
+  ICourseData,
+  ILink,
+  IReview,
+} from "../types/course.types";
+
+const reviewSchema = new Schema<IReview>({
+  user: Object,
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  comment: String,
+});
+
+const linkSchema = new Schema<ILink>({
+  title: String,
+  url: String,
+});
+
+const commentSchema = new Schema<IComment>({
+  user: Object,
+  comment: String,
+  commentReplies: [Object],
+});
+
+const courseDataSchema = new Schema<ICourseData>({
+  videoUrl: String,
+  title: String,
+  videoSection: String,
+  description: String,
+  videoLength: Number,
+  videoPlayer: String,
+  links: [linkSchema],
+  suggestion: String,
+  questions: [commentSchema],
+});
+
+const courseSchema = new Schema<ICourse>({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  estimatedPrice: {
+    type: Number,
+  },
+  thumbnail: {
+    public_id: {
+      type: String,
+    },
+    url: {
+      type: String,
+    },
+  },
+  tags: {
+    type: String,
+    required: true,
+  },
+  level: {
+    type: String,
+    required: true,
+  },
+  demoUrl: {
+    type: String,
+    required: true,
+  },
+  benefits: [{ title: String }],
+  prerequisites: [{ title: String }],
+  reviews: [reviewSchema],
+  courseData: [courseDataSchema],
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+  purchased: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const Course: Model<ICourse> = model("Course", courseSchema);
+
+export { Course };
