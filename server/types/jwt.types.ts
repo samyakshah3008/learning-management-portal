69874e313abe7837764716fa6551272eb8254f69
Samyak Shah <samyakshah3008@gmail.com>
2024-09-06 20:38:32 +0530

    feat: jwt types add

---
+++ server/types/jwt.types.ts
@@ -0,0 +1,9 @@
+interface ITokenOptions {
+  expires: Date;
+  maxAge: number;
+  httpOnly: boolean;
+  sameSite: "lax" | "strict" | "none" | undefined;
+  secure?: boolean;
+}
+
+export { ITokenOptions };
