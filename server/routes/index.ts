681dcc4fae718a7de6fff6ecf051d38b2ac54dde
Samyak Shah <samyakshah3008@gmail.com>
2024-09-06 20:37:53 +0530

    auth and user routes wire

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,8 +2,10 @@ import { Router } from "express";
 
 const router = Router();
 
+import authRouter from "./auth.route";
 import userRouter from "./user.route";
 
-router.use("/", userRouter);
+router.use("/auth", authRouter);
+router.use("/user", userRouter);
 
 export default router;
