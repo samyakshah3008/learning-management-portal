c391ae00dc39e45ab0ba6cc032accc39b2e78c1c
Samyak Shah <samyakshah3008@gmail.com>
2024-09-06 22:53:53 +0530

    course route wired

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -3,9 +3,11 @@ import { Router } from "express";
 const router = Router();
 
 import authRouter from "./auth.route";
+import courseRouter from "./course.route";
 import userRouter from "./user.route";
 
 router.use("/auth", authRouter);
 router.use("/user", userRouter);
+router.use("/course", courseRouter);
 
 export default router;
