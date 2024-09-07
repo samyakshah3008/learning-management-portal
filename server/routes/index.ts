a62074b8f4f9575c50efb94ab35110d22d31f230
Samyak Shah <samyakshah3008@gmail.com>
2024-09-07 23:51:58 +0530

    feat: order, notification, analytics routers wired

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,12 +2,18 @@ import { Router } from "express";
 
 const router = Router();
 
+import analyticsRouter from "./analytics.route";
 import authRouter from "./auth.route";
 import courseRouter from "./course.route";
+import notificationRouter from "./notification.route";
+import orderRouter from "./order.route";
 import userRouter from "./user.route";
 
 router.use("/auth", authRouter);
 router.use("/user", userRouter);
 router.use("/course", courseRouter);
+router.use("/order", orderRouter);
+router.use("/notification", notificationRouter);
+router.use("/analytics", analyticsRouter);
 
 export default router;
