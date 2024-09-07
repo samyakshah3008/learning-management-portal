027e2ba7a3d25bb061fccaf6cff4aa61f78b65dd
Samyak Shah <samyakshah3008@gmail.com>
2024-09-07 16:31:36 +0530

    feat: course routes wired

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,8 +1,22 @@
 import { Router } from "express";
-import { editCourse, uploadCourse } from "../controllers/course.controller";
+import {
+  addAnswer,
+  addQuestion,
+  addReplyToReview,
+  addReview,
+  editCourse,
+  getAllCourses,
+  getCourseByBuyer,
+  getParticularCourse,
+  uploadCourse,
+} from "../controllers/course.controller";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 
 const router = Router();
+
+router.get("/get-course/:id", getParticularCourse);
+router.get("/get-courses", getAllCourses);
+router.get("/get-course-content/:id", isAuthenticated, getCourseByBuyer);
 
 router.post(
   "/upload-course",
@@ -16,6 +30,15 @@ router.put(
   isAuthenticated,
   authorizeRoles("admin"),
   editCourse
+);
+router.put("/add-question", isAuthenticated, addQuestion);
+router.put("/add-answer", isAuthenticated, addAnswer);
+router.put("/add-review/:id", isAuthenticated, addReview);
+router.put(
+  "/add-review-reply",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  addReplyToReview
 );
 
 export default router;
