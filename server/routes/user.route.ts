8328d127cc8cc25d7528a5885d332552b92a5c4c
Samyak Shah <samyakshah3008@gmail.com>
2024-09-07 23:52:35 +0530

    updates in user routes

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,17 +1,39 @@
 import { Router } from "express";
 import {
+  deleteUser,
+  fetchAllUsers,
   getUserInfo,
   updateAvatar,
   updatePassword,
   updateUserInfo,
+  updateUserRole,
 } from "../controllers/user.controller";
-import { isAuthenticated } from "../middleware/auth";
+import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 
 const router = Router();
 
 router.get("/get-user-details", isAuthenticated, getUserInfo);
+router.get(
+  "/admin/get-users",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  fetchAllUsers
+);
 router.put("/update-info", isAuthenticated, updateUserInfo);
 router.put("/update-password", isAuthenticated, updatePassword);
 router.put("/update-avatar", isAuthenticated, updateAvatar);
+router.put(
+  "/update-user-role",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  updateUserRole
+);
+
+router.delete(
+  "/delete-user/:id",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  deleteUser
+);
 
 export default router;
