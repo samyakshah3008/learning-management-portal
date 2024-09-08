f76f1696a7e9f7e04083bcb0692c7e6c35bfaf36
Samyak Shah <samyakshah3008@gmail.com>
2024-09-08 16:34:19 +0530

    feat: layout schema design

---
+++ server/models/layout.model.ts
@@ -0,0 +1,31 @@
+import { model, Schema } from "mongoose";
+import { BannerImage, Category, FaqItem, Layout } from "../types/layout.types";
+
+const faqSchema = new Schema<FaqItem>({
+  question: { type: String },
+  answer: { type: String },
+});
+
+const categorySchema = new Schema<Category>({
+  title: { type: String },
+});
+
+const bannerImageSchema = new Schema<BannerImage>({
+  public_id: { type: String },
+  url: { type: String },
+});
+
+const layoutSchema = new Schema<Layout>({
+  type: { type: String },
+  faq: [faqSchema],
+  categories: [categorySchema],
+  banner: {
+    image: bannerImageSchema,
+    title: { type: String },
+    subTitle: { type: String },
+  },
+});
+
+const Layout = model<Layout>("Layout", layoutSchema);
+
+export { Layout };
